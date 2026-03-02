7273d066f5f8847b11e45b7e8e3aa69f068dc3e1
beau-hoss <shapeswerke@gmail.com>
2026-03-01 23:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-async function login() {
-  const badge = document.getElementById("badge").value.trim();
+document.addEventListener("DOMContentLoaded", () => {
 
-  const res = await fetch("/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ badge })
-  });
+  const loginBtn = document.getElementById("loginBtn");
 
-  const data = await res.json();
+  if (loginBtn) {
+    loginBtn.addEventListener("click", async () => {
+      const badge = document.getElementById("badge").value.trim();
 
-  if (res.ok && data.success) {
-    localStorage.setItem("badge", badge);
-    window.location.href = "dashboard.html";
-  } else {
-    document.getElementById("error").innerText = "Unauthorized Badge #";
+      const res = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ badge })
+      });
+
+      const data = await res.json();
+
+      if (res.ok && data.success) {
+        localStorage.setItem("badge", badge);
+        window.location.href = "/dashboard.html";
+      } else {
+        document.getElementById("error").innerText = "Unauthorized Badge #";
+      }
+    });
   }
-}
+
+});

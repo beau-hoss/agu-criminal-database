45cfd8acffd4c69e5f46fb8f0999e22421c597c1
beau-hoss <shapeswerke@gmail.com>
2026-03-01 23:58:28 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -3,7 +3,9 @@ module.exports = (req, res) => {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
-  const approvedBadges = ["918", "1002", "2001", "3007"];
+  const approvedBadges = ["918", "6039", "416", "10194", "928", "491", "824", "515", "741", "9951", "491", "304", "392", "404", "1738", "0420", "7201", 
+                         "332", "454", "8691", "559", "1919", "181", "1738", "231", "90210", "47884", "678", "6767", "568", "2006", "918", "3690", 
+                         "999", "10194", "6039", "491", "6741", "504", "928", ];
   const { badge } = req.body;
 
   if (approvedBadges.includes(String(badge).trim())) {
@@ -12,4 +14,5 @@ module.exports = (req, res) => {
     return res.status(401).json({ success: false });
   }
 };
+
 

3fa302144ec65404ada02d796e5f505c4ed1abb0
beau-hoss <shapeswerke@gmail.com>
2026-03-05 04:35:26 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -6,6 +6,7 @@ module.exports = (req, res) => {
 
   const approvedBadges = ["416",
     "10194",
+    "148",                      
     "4800",
     "928",
     "148",                          
@@ -52,6 +53,7 @@ module.exports = (req, res) => {
 
   return res.status(401).json({ success: false });
 };
+
 
 
 
